29eb1fe8151e543703ba606aa290f467ad1ac406
Li Chi Hsiao <siao.lichi@gmail.com>
2020-05-01 18:25:38 +0200

    finish alert.test.js

--- client/src/actions/alert.test.js
+++ client/src/actions/alert.test.js
@@ -1,2 +1,10 @@
-import alert from './alert';
-test('', () => {});
+import { setAlert } from './alert';
+import { storeFactory } from '../utils';
+
+test('show error message correctly', () => {
+  const store = storeFactory();
+  store.dispatch(setAlert('Alert Message', 'success'));
+  const newState = store.getState();
+  expect(newState.alert[0].msg).toBe('Alert Message');
+  expect(newState.alert[0].alertType).toBe('success');
+});
