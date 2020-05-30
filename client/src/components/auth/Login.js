9141ed25759886d1247e95bc43566ca341da5cf1
Li Chi Hsiao <siao.lichi@gmail.com>
2020-05-30 23:13:27 +0200

    test

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -43,7 +43,7 @@ const Login = ({
     e.preventDefault();
     await login({ email, password });
   };
-  if (redirect) return <Spinner />;
+  if (redirect) return <Redirect to='/' />;
   if (isAuthenticated) {
     return <Redirect to='/dashboard' />;
   }
