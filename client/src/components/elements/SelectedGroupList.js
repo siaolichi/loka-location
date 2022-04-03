ca32d6bc1716f47801ae8be3b372416d4ae89b0f
Lichi <siao.lichi@gmail.com>
2022-04-03 12:56:08 +0800

    remove stagger animation

--- client/src/components/elements/SelectedGroupList.js
+++ client/src/components/elements/SelectedGroupList.js
@@ -1,41 +1,33 @@
 /*eslint-disable react-hooks/exhaustive-deps*/
-import React, { useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
 import './SelectedGroupList.scss';
 
-import { staggerIn, staggerOut } from '../../utils';
 import { connect } from 'react-redux';
 
 const GroupList = ({ modal, setModal }) => {
-  const selectedRef = useRef(null);
-  const hideAll = (groupId) => {
-    staggerOut([...selectedRef.current.childNodes], () => {
-      setModal((m) => ({ ...m, currentGroupId: groupId }));
-    });
-  };
-  useEffect(() => {
-    if (modal.selected.length > 0) {
-      staggerIn(selectedRef.current.childNodes);
-    }
-  }, [modal.other]);
+	const selectedRef = useRef(null);
+	const hideAll = (groupId) => {
+		setModal((m) => ({ ...m, currentGroupId: groupId }));
+	};
 
-  return (
-    <div id='selected-group-list' className='fade-in'>
-      <div className='list-wrapper' ref={selectedRef}>
-        <div className='title'>Ｍy Favorite</div>
-        {modal.selected.map((group, index) => (
-          <button
-            key={index}
-            className='group-content button'
-            onClick={(e) => {
-              hideAll(group._id);
-            }}
-          >
-            {group.name}
-          </button>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div id='selected-group-list' className='fade-in'>
+			<div className='list-wrapper' ref={selectedRef}>
+				<div className='title'>Ｍy Favorite</div>
+				{modal.selected.map((group, index) => (
+					<button
+						key={index}
+						className='group-content button'
+						onClick={(e) => {
+							hideAll(group._id);
+						}}
+					>
+						{group.name}
+					</button>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default connect(null, null)(GroupList);
